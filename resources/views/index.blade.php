8929ec23c23c24f474f6d5b88eed654581b7b1df
Michal Grabowski <Miriloth@gmail.com>
2017-05-16 13:35:30 +0200

    Add basically configured frontend

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./style/blueprint.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ config('app.name') }}</title>
 </head>
 <body>
-    Welcome
+<div class="app"></div>
 </body>
+<script src="./bundle.js"></script>
 </html>
